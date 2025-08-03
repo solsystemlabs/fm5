fbe8ef21b1fbc2ee82b89384e4f3866f13c2a339
teernisse <teernisse@gmail.com>
2025-08-02 22:36:26 -0400

    add login form from shadcn

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,8 +1,9 @@
+import { LoginForm } from "./login-form";
+
 export function LoginPage() {
   return (
-    <div className="flex w-full h-full flex-col">
-      <div className="text-xl">Login Page</div>
-      <div className="flex justify-center items-center h-full w-full flex-col"></div>
+    <div className="flex h-full w-full justify-center items-center">
+      <LoginForm />
     </div>
   );
 }

9ca4e66e9fcd53c63101b5807a9c06be08ee2585
teernisse <teernisse@gmail.com>
2025-08-23 19:31:40 -0400

    improve small layout of filament table

--- src/components/FilamentsTable.tsx
+++ src/components/FilamentsTable.tsx
@@ -9,10 +9,8 @@ import {
   TableBody,
   TableHeader,
 } from "react-aria-components";
-
-function classNames(...classes: (string | boolean | undefined)[]): string {
-  return classes.filter(Boolean).join(' ')
-}
+import FMTableCell from "./table/FMTableCell";
+import clsx from "clsx";
 
 export default function FilamentsTable({
   data,
@@ -51,14 +49,14 @@ export default function FilamentsTable({
           {data.map((filament, filamentIdx) => (
             <Row key={filament.id}>
               <Cell
-                className={classNames(
-                  filamentIdx === 0 ? '' : 'border-t border-transparent',
-                  'relative py-4 pr-3 pl-4 text-sm sm:pl-6',
+                className={clsx(
+                  filamentIdx === 0 ? "" : "border-t border-transparent",
+                  "relative py-4 pr-3 pl-4 text-sm sm:pl-6",
                 )}
               >
                 <div className="flex items-center gap-x-3">
                   <ColorSwatch
-                    className="h-6 w-6 rounded-sm flex-shrink-0"
+                    className="h-8 w-8 rounded-sm flex-shrink-0 shadow-[0_4px_6px_0_rgba(50,50,50,0.25)]"
                     color={filament.color}
                   />
                   <div className="font-medium text-gray-900 dark:text-white">
@@ -66,66 +64,52 @@ export default function FilamentsTable({
                   </div>
                 </div>
                 <div className="mt-1 flex flex-col text-gray-500 sm:block lg:hidden dark:text-gray-400">
-                  <span>
-                    {filament.brandName} / {filament.Material.name}
-                  </span>
-                  <span className="hidden sm:inline">·</span>
+                  <span>{filament.brandName}</span>
+                  <span className="hidden sm:inline text-gray-300"> | </span>
+                  <span>{filament.Material.name}</span>
+                  <span className="hidden sm:inline text-gray-300"> | </span>
                   <span>{filament.diameter}mm</span>
                 </div>
                 {filamentIdx !== 0 ? (
                   <div className="absolute -top-px right-0 left-6 h-px bg-gray-200 dark:bg-white/10" />
                 ) : null}
               </Cell>
+              <FMTableCell>{filament.brandName}</FMTableCell>
+              <FMTableCell>{filament.Material.name}</FMTableCell>
+              <FMTableCell>{filament.diameter}mm</FMTableCell>
               <Cell
-                className={classNames(
-                  filamentIdx === 0 ? '' : 'border-t border-gray-200 dark:border-white/10',
-                  'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell dark:text-gray-400',
-                )}
-              >
-                {filament.brandName}
-              </Cell>
-              <Cell
-                className={classNames(
-                  filamentIdx === 0 ? '' : 'border-t border-gray-200 dark:border-white/10',
-                  'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell dark:text-gray-400',
-                )}
-              >
-                {filament.Material.name}
-              </Cell>
-              <Cell
-                className={classNames(
-                  filamentIdx === 0 ? '' : 'border-t border-gray-200 dark:border-white/10',
-                  'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell dark:text-gray-400',
-                )}
-              >
-                {filament.diameter}mm
-              </Cell>
-              <Cell
-                className={classNames(
-                  filamentIdx === 0 ? '' : 'border-t border-gray-200 dark:border-white/10',
-                  'px-3 py-3.5 text-sm text-gray-500 dark:text-gray-400',
+                className={clsx(
+                  filamentIdx === 0
+                    ? ""
+                    : "border-t border-gray-200 dark:border-white/10",
+                  "px-3 py-3.5 text-sm text-gray-500 dark:text-gray-400",
                 )}
               >
                 <div className="sm:hidden">${filament.cost}</div>
                 <div className="hidden sm:block">${filament.cost}</div>
               </Cell>
               <Cell
-                className={classNames(
-                  filamentIdx === 0 ? '' : 'border-t border-gray-200 dark:border-white/10',
-                  'px-3 py-3.5 text-sm text-gray-500 dark:text-gray-400',
+                className={clsx(
+                  filamentIdx === 0
+                    ? ""
+                    : "border-t border-gray-200 dark:border-white/10",
+                  "px-3 py-3.5 text-sm text-gray-500 dark:text-gray-400",
                 )}
               >
                 {filament.grams}g
               </Cell>
               <Cell
-                className={classNames(
-                  filamentIdx === 0 ? '' : 'border-t border-transparent',
-                  'relative py-3.5 pr-4 pl-3 text-right text-sm font-medium sm:pr-6',
+                className={clsx(
+                  filamentIdx === 0 ? "" : "border-t border-transparent",
+                  "relative py-3.5 pr-4 pl-3 text-right text-sm font-medium sm:pr-6",
                 )}
               >
                 <div className="text-gray-500 dark:text-gray-400">
                   {filament.Models && filament.Models.length > 0 ? (
-                    <span>{filament.Models.length} model{filament.Models.length !== 1 ? 's' : ''}</span>
+                    <span>
+                      {filament.Models.length} model
+                      {filament.Models.length !== 1 ? "s" : ""}
+                    </span>
                   ) : (
                     <span>No models</span>
                   )}

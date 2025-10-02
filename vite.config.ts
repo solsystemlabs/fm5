ca9efdd90539d1e8f177a516f79a2bd9d6bf466c
teernisse <teernisse@gmail.com>
2025-10-02 14:13:48 -0400

    manual attempt

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   plugins: [
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    tanstackStart(),
     tailwindcss(),
-    tanstackStart({
-      target: 'cloudflare-module',
-      customViteReactPlugin: true,
-    }),
     viteReact(),
   ],
   test: {
